dc3f5ce89598383b15a3761d6625d3944160c456
sebastian ojeda <sebastianojeda>
2014-11-05 12:26:35 -0800

    put stuff in my post file

---
+++ view/form.php
@@ -0,0 +1,12 @@
+<h1>Create Blog Post</h1>
+
+<form>
+	<div>
+		<label for="title">Title:</label>
+		<input	type="" name="title"/>
+	</div>
+	<div>
+		<label for="post">Post:</label>
+		<textarea name="post"></textarea>
+	</div>
+</form>
--- view/navigation.php
+++ view/navigation.php
@@ -1,5 +1,9 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+?>
+
 <nav>
 	<ul>
-		<li><a href="/sebBlog/post.php">Blog post form</a></li>
+		<li><a href="<?php echo $path . "post.php"?>">Blog post form</a></li>
 	</ul>
 </nav>
