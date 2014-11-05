ae0c89872cbf744e160ce58196e0ea9fb1dd4294
sebastian ojeda <sebastianojeda>
2014-11-05 12:04:59 -0800

    added footer, header, and a link

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
- <!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
 <?php
-
+	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/view/navigation.php");
 	require_once(__DIR__ . "/controller/create-db.php");
-	
-
+	require_once(__DIR__ . "/view/footer.php");
 ?>
-
-</body>
-</html>
---
+++ view/navigation.php
@@ -0,0 +1,5 @@
+<nav>
+	<ul>
+		<li><a href="/sebBlog/post.php">Blog post form</a></li>
+	</ul>
+</nav>
