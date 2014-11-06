3683ec65013570f03847410c20b6e9013b65c687
sebastian ojeda <sebastianojeda>
2014-11-06 12:16:48 -0800

    added ipad media query

--- view/header.php
+++ view/header.php
@@ -1,6 +1,7 @@
  <!DOCTYPE html>
 <html>
 <head>
+<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 <link rel="stylesheet" type="text/css" href="css/main.css">
 	<title></title>
 </head>
