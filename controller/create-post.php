d060525ff1020b372511e4087fa816802350c9a1
sebastian ojeda <sebastianojeda>
2014-11-12 12:11:36 -0800

    amazing

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,7 +1,14 @@
 <?php
-//THIS ARE VARIABLES THAT HOLD A FUNCTION TO FILTER OR POSTS
-$title = filer_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+require_once(__DIR__ . "/../model/database.php");
+// this conection variable is conecting me to my database file
+	$connection = new mysqli($host, $username, $password, $database);
+
+//These are variables that hold a function to filter our posts
+$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input(INPUT_POST , "post", FILTER_SANITIZE_STRING);
 //these echo's output if our variables hav been filtered.
-echo "<p>Title: $title</p>";
-echo "<p>Post: $post</p>";
+	echo "<p>Title: $title</p>";
+	echo "<p>Post: $post</p>";
+
+	$connection->close();
+
--- view/form.php
+++ view/form.php
@@ -1,6 +1,13 @@
-<h1>Create Blog Post</h1>
+<!--This require_once is conecting my config.php to my form.php-->
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+?>
 
-<form>
+<h1>Create Blog Post</h1>
+                            <!--This php code is taking me to my create-post.php 
+                            file so that when i submit something on my blog that 
+                            submition is stored in my create-post.php file-->
+<form method="post" action="<?php echo $path . "controller/create-post.php";?>">
 	<div>
 		<label for="title">Title:</label>
 		<input	type="" name="title"/>
