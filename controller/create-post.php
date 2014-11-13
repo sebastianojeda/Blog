a0e9b49f75a08b7ee906c0a91a4d821f1e06e64a
sebastian ojeda <sebastianojeda>
2014-11-13 11:46:19 -0800

    able to post

--- controller/create-post.php
+++ controller/create-post.php
@@ -6,9 +6,19 @@ require_once(__DIR__ . "/../model/database.php");
 //These are variables that hold a function to filter our posts
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 $post = filter_input(INPUT_POST , "post", FILTER_SANITIZE_STRING);
-//these echo's output if our variables hav been filtered.
-	echo "<p>Title: $title</p>";
-	echo "<p>Post: $post</p>";
+	
+	// this query variable is allowing me to store all of my blog Posts in to 
+	//my XAMPP database 
+	$query = $connection->query("INSERT INTO posts SET title = 
+		'$title', post = '$post'");
+
+	//This if esle statement is checking to see if my query is working 
+	if($query) {
+		echo "<p>Successfuly inserted post: $title</p>";
+	} 
+	else{
+		echo "<p>$connection->error</p>";
+	}
 
 	$connection->close();
 
--- view/header.php
+++ view/header.php
@@ -2,6 +2,8 @@
 <html>
 <head>
 <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 	<title></title>
 </head>
